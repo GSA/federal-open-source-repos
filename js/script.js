01bffb7d82f34604836ae98922766bb5e8cf66e0
Chris Barna <chris@unbrain.net>
2012-06-27 16:45:34 -0700

    Improve aesthetics.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,26 @@
 $(document).ready(function () {
 	$.get("http://registry.usa.gov/accounts.json?service_id=github", function (data) {
 		for (var i = 0; i < data.accounts.length; i += 1) {
-			$("#main").append("<h2>"+data.accounts[i].organization+"</h2><ul id=\""+data.accounts[i].account+"\"></ul>");
+			$("#main").append("<div id=\""+data.accounts[i].account+"\" class=\"organization\"><h2>"+data.accounts[i].organization+"</h2></div>");
 			$.getJSON("https://api.github.com/users/"+data.accounts[i].account+"/repos?callback=?", function (repo_data) {
 				for (var j = 0; j < repo_data.data.length; j += 1) {
-					$("#"+repo_data.data[j].owner.login.toLowerCase()).append("<li>"+repo_data.data[j].name+"</li>");
+					var appendStr = "<div class=\"project\">";
+
+					if (repo_data.data[j].language !== null) {
+						appendStr += "<span class=\"language label\">"+repo_data.data[j].language+"</span>";
+					}
+
+					appendStr += "<span class=\"forks\"><i class=\"icon-random\"></i> "+repo_data.data[j].forks+"</span>";
+					appendStr += "<span class=\"watchers\"><i class=\"icon-eye-open\"></i> "+repo_data.data[j].watchers+"</span>";
+
+					appendStr += "<h3><a href=\""+repo_data.data[j].html_url+"\">"+repo_data.data[j].name+"</a></h3>";
+
+					if (repo_data.data[j].description !== null) {
+						appendStr += "<p>"+repo_data.data[j].description+"</p>";
+					}
+
+					$("#"+repo_data.data[j].owner.login.toLowerCase())
+						.append(appendStr);
 				}
 			});
 		}
